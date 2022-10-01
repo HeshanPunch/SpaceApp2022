2a3088700caf6ae23d303930c4e6d9e7e13aba30
yugykim <yugyeongk92@gmail.com>
2022-10-01 14:24:55 -0600

    setted up kaboom with NPM and Parcel

---
+++ SpaceApp22/src/main.js
@@ -0,0 +1,17 @@
+import kaboom from "kaboom";
+
+// initialize context
+const k = kaboom({
+  
+})
+
+k.scene('main', () => [
+  k.add([
+    k.text('Hello World', 32),
+    k.pos(k.width() * 0.5, k.height() * 0.5),
+    k.color(1,1,1,1),
+    k.origin('center')
+  ])
+])
+
+k.go('main')
